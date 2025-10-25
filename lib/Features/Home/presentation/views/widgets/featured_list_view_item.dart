4da06c9c0ee3f8f65c207bab400a45a5a61499b5
hp <mohamedoraby0500@gmail.com>
2025-10-25 13:51:30 +0300

    create featured list view

---
+++ lib/Features/Home/presentation/views/widgets/featured_list_view_item.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/core/utils/assets_data.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.red,
+          image: DecorationImage(image: AssetImage(AssetsData.testImage)),
+        ),
+      ),
+    );
+  }
+}
