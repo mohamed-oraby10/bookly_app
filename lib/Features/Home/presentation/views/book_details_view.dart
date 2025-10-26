8c359ce45c9bb46f3b4faf21481667e06062754b
hp <mohamedoraby0500@gmail.com>
2025-10-26 20:36:39 +0300

    create Book Details Body

---
+++ lib/Features/Home/presentation/views/book_details_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/Features/Home/presentation/views/widgets/book_details_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsBody(),
+    );
+  }
+}
