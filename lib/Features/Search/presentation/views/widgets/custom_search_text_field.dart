6675d73bd979705637c664839c406a55cfa51264
hp <mohamedoraby0500@gmail.com>
2025-10-27 16:35:02 +0300

    create custom search text field

---
+++ lib/Features/Search/presentation/views/widgets/custom_search_text_field.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        focusedBorder: buildOutlineInputBorder(),
+        enabledBorder: buildOutlineInputBorder(),
+        suffixIcon: Opacity(
+          opacity: .8,
+          child: Icon(FontAwesomeIcons.magnifyingGlass, size: 22),
+        ),
+        hintText: 'Search',
+      ),
+    );
+  }
+
+  OutlineInputBorder buildOutlineInputBorder() {
+    return OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.white),
+      borderRadius: BorderRadius.circular(16),
+    );
+  }
+}
