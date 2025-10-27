53bb5b68db46fb6ecdfb19aec72a69526ab2e701
hp <mohamedoraby0500@gmail.com>
2025-10-27 15:13:39 +0300

    create similar books list view

--- lib/Features/Home/presentation/views/widgets/book_details_body.dart
+++ lib/Features/Home/presentation/views/widgets/book_details_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/Features/Home/presentation/views/widgets/book_rating.
 import 'package:bookly_app/Features/Home/presentation/views/widgets/books_action.dart';
 import 'package:bookly_app/Features/Home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/Home/presentation/views/widgets/custom_book_imge.dart';
+import 'package:bookly_app/Features/Home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -41,6 +42,17 @@ class BookDetailsBody extends StatelessWidget {
             const BookRating(mainAxisAlignment: MainAxisAlignment.center),
             const SizedBox(height: 37),
             const BooksAction(),
+            const SizedBox(height: 50),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'You can also like',
+                style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const SimilarBooksListView(),
+            const SizedBox(height: 40),
           ],
         ),
       ),
---
+++ lib/Features/Home/presentation/views/widgets/similar_books_list_view.dart
@@ -0,0 +1,22 @@
+import 'package:bookly_app/Features/Home/presentation/views/widgets/custom_book_imge.dart';
+import 'package:flutter/material.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .15,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: CustomBookItem(),
+          );
+        },
+      ),
+    );
+  }
+}
